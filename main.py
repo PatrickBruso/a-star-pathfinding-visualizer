240c8837bd919602c85daf1d8219c432ba2f72d9
Patrick <pkbruso@gmail.com>
2021-11-05 13:17:00 -0700

    Added Node class and definitions

--- main.py
+++ main.py
@@ -24,4 +24,34 @@ GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
 
+class Node:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
 
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == PURPLE
+
+    def reset(self):
+        self.color == WHITE
