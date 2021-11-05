ae59809410262eedc0c644df5cda7eba30bd8c9a
Patrick <pkbruso@gmail.com>
2021-11-05 15:06:43 -0700

    Finished functions for Node class

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
-ORANGE= (255, 165, 0)
+ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
@@ -51,7 +51,32 @@ class Node:
         return self.color == ORANGE
 
     def is_end(self):
-        return self.color == PURPLE
+        return self.color == TURQUOISE
 
     def reset(self):
         self.color == WHITE
+
+    def make_closed(self):
+        self.color == RED
+
+    def make_open(self):
+        self.color == GREEN
+
+    def make_barrier(self):
+        self.color == BLACK
+
+    def make_end(self):
+        self.color == TURQUOISE
+
+    def make_path(self):
+        self.color == PURPLE
+
+    def draw(self, win):
+        pg.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+
