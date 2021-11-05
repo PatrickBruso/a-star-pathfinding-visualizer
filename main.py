64d5c10618c3351f3c60c26b3ed0145071474849
Patrick <pkbruso@gmail.com>
2021-11-05 13:16:42 -0700

    Added color constants

--- main.py
+++ main.py
@@ -11,4 +11,17 @@ WIDTH = 800
 WIN = pg.display.set_mode((WIDTH, WIDTH))
 pg.display.set_caption("A* Path Finding Algorithm")
 
+# Color constants
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE= (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+
 
