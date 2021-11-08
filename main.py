6f13c188cce91003cfd2d7e7973d042ad1f79c18
Patrick <pkbruso@gmail.com>
2021-11-07 18:36:24 -0800

    Added function to get position of mouse click

--- main.py
+++ main.py
@@ -116,3 +116,16 @@ def draw(win, grid, rows, width):
 
     draw_grid(win, rows, width)
     pg.display.update()
+
+
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+
+    return row, col
+
+
+
